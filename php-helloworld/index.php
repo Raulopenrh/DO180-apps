7413008834181100403dbc027808d07cf777f244
Student User <student@workstation.lab.example.com>
2022-11-20 17:05:00 -0500

    Arreglado

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
 print "Otro texto mas!\n";
-=======
 print "A change is a coming!\n";
->>>>>>> e51cf4cf9323ba07743846798e7f34cbf00eea26
 ?>
